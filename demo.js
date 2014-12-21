b97de4ed1734229bd1943a50b292e2df461256af
Sašo Cvitkovič <sc5879@student.uni-lj.si>
2014-12-21 21:29:51 +0000

    manjsi popravek

--- demo.js
+++ demo.js
@@ -9,6 +9,8 @@ var uporabnikiMes=[];
 var uporabnikiLeto=[];
 var stevec=3;
 var visina1;
+var trenutnateza;
+var besedilo;
 
 
 var username = "ois.seminar";
@@ -227,9 +229,11 @@ function pridobiId1(){
 }
 
 function preberiMeritveVitalnihZnakov() {
-	sessionId = getSessionId1();	
+	sessionId = getSessionId();	
 	var ime= $('#uporabnik :selected').text();
-	var ehrId = pridobiId();
+	var ehrId = pridobiId1();
+	$('#rezultati2').empty();
+	$('#rezultati3').empty();
 
 	if (!ehrId || ehrId.trim().length == 0) {
 		$("#rezultati1").html("<span class='obvestilo label label-warning fade-in'>Prosim vnesite zahtevan podatek!");
@@ -247,12 +251,14 @@ function preberiMeritveVitalnihZnakov() {
 					    headers: {"Ehr-Session": sessionId},
 					    success: function (res) {
 					    	if (res.length > 0) {
-						    	var results = "<table class='table table-striped table-hover'><tr><th>Datum in ura</th><th class='text-right'>Telesna teža</th></tr>";
-						        for (var i in res) {
-						            results += "<tr><td>" + res[i].time + "</td><td class='text-right'>" + res[i].weight + " " 	+ res[i].unit + "</td>";
-						        }
-						        results += "</table>";
-						        $("#rezultati1").append(results);
+					    		trenutnateza=res[0].weight;
+						    	var zgubljenaTeza=res[res.length-1].weight-res[0].weight;
+						    	if(zgubljenaTeza<0){
+						    		zgubljenaTeza=res[0].weight-res[res.length-1].weight;
+						    		besedilo="Pridobili ste "+zgubljenaTeza+" kilogramov.";
+						    	}else{
+						    		besedilo="Od zacetka ste izgubili"+zgubljenaTeza+" kilogramov.";
+						    	}
 					    	} else {
 					    		$("#rezultati1").html("<span class='obvestilo label label-warning fade-in'>Ni podatkov!</span>");
 					    	}
@@ -287,7 +293,22 @@ function preberiMeritveVitalnihZnakov() {
 					    headers: {"Ehr-Session": sessionId},
 					    success: function (res) {
 					    	if (res.length > 0) {
-						    	visina1=res[0].heigth;
+						    	visina1=res[0].height;
+						    	alert(visina1);
+						    	var itm=(trenutnateza/((visina1/100)*(visina1/100)));
+						    	besedilo=besedilo + "\nVas trenutni itm je "+itm;
+						    	if(itm<20){
+						    		besedilo=besedilo+". Glede na mejnik ste v podhranjenosti.";
+						    	}else if(itm<25){
+						    		besedilo=besedilo+". Glede na mejnik imate normalno tezo.";
+						    	}else if(itm<30){
+						    		besedilo=besedilo+". Glede na mejnik ste v debelosti stopnje 1.";
+						    	}else if(itm<40){
+						    		besedilo=besedilo+". Glede na mejnik ste v debelosti stopnje 2.";
+						    	}else{
+						    		besedilo=besedilo+". Glede na mejnik ste v debelosti stopnje 3.";
+						    	}
+						    	$('#rezultati2').append(besedilo);
 					    	} else {
 					    		$("#rezultati1").html("<span class='obvestilo label label-warning fade-in'>Ni podatkov!</span>");
 					    	}
@@ -324,6 +345,10 @@ function preberiMeritveVitalnihZnakov() {
 						    	var results = "<table class='table table-striped table-hover'><tr><th>Datum in ura</th><th class='text-right'>utrip</th></tr>";
 						        for (var i in res) {
 						            results += "<tr><td>" + res[i].time + "</td><td class='text-right'>" + res[i].pulse + " " 	+ res[i].unit + "</td>";
+						            if(res[i].pulse>+175){
+						            	var opozorilo="Pri merjenju dne "+res[i].time+" je zabelezen visok srcni utrip.";
+						            	$("#rezultati3").html("<span class='obvestilo label label-warning fade-in'>"+opozorilo+"</span>");
+						            }
 						        }
 						        results += "</table>";
 						        $("#rezultati1").append(results);
@@ -338,7 +363,7 @@ function preberiMeritveVitalnihZnakov() {
 					});					
 	    	},
 	    	error: function(err) {
-	    		$("#rezultati").html("<span class='obvestilo label label-danger fade-in'>Napaka '" + JSON.parse(err.responseText).userMessage + "'!");
+	    		$("#rezultati1").html("<span class='obvestilo label label-danger fade-in'>Napaka '" + JSON.parse(err.responseText).userMessage + "'!");
 				console.log(JSON.parse(err.responseText).userMessage);
 	    	}
 	    	
